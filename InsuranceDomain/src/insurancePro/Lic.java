f173c332d6b4eddde00c5db269f4d2f708eca9c4
PratikJagtap89 <112230443+PratikJagtap89@users.noreply.github.com>
2022-09-14 11:33:48 +0530

    Update Lic.java

--- InsuranceDomain/src/insurancePro/Lic.java
+++ InsuranceDomain/src/insurancePro/Lic.java
@@ -5,7 +5,7 @@ public class Lic {
 	public static void main(String[] args) {
 	
 		 
-		System.out.println ("we have to create one project ");
+		System.out.println ("we have to create one  ");
 
 	}
 
